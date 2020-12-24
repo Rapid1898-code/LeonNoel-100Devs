77ccea4eb915287ef072daadee6cca04c7c07421
Rapid1898 <markuspolzer@gmx.at>
2020-12-24 23:50:26 +0100

    Update

--- thejsway/work/js/temp.js
+++ thejsway/work/js/temp.js
@@ -1,3 +1,7 @@
-const groceryList = ['orange juice', 'bananas', 'coffee beans', 'brown rice', 'pasta', 'coconut oil', 'plantains'];
+let arrays = [[1, 2, 3], [4, 5], [6]];
+// Your code here.
+// → [1, 2, 3, 4, 5, 6]
 
-
+arr2 = arrays[0].concat(arrays[1])
+arrErg = arr2.concat(arrays[2])
+console.log(arrErg)
