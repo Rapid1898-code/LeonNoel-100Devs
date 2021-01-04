30c28e888f51d39db6c7a9d4c94f7a300636119b
Rapid1898 <markuspolzer@gmx.at>
2021-01-04 23:37:45 +0100

    Update

--- javascript30/05FlexGallery/js/main.js
+++ javascript30/05FlexGallery/js/main.js
@@ -4,11 +4,18 @@ panels.forEach(panel => panel.addEventListener("click",toggleOpen))
 panels.forEach(panel => panel.addEventListener("transitionend",toggleActive))
 
 function toggleOpen() {
-    this.classList.toggle("open")
+    panels.forEach((elem) => {
+        elem.classList.remove("open")
+    })
+    this.classList.add("open")
+
 }
 
 function toggleActive(e) {
     if (e.propertyName.includes("flex")) {
-        this.classList.toggle("open-active")
+        panels.forEach((elem) => {
+            elem.classList.remove("open-active")
+        })
+        this.classList.add("open-active")
     }
 }
