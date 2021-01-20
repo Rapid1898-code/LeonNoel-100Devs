bc2f4dd3965ce3d486d69fe48d9f221604586247
Rapid1898 <markuspolzer@gmx.at>
2021-01-20 22:28:23 +0100

    Update

--- class17/alien-rangers-review/js/main.js
+++ class17/alien-rangers-review/js/main.js
@@ -1,9 +1,20 @@
 //Arrays
 
 //Create and array of tv shows. Loop through and print each show to the console
+showsArr = ["Terminator","Rambo","Es"]
+showsArr.forEach((elem,idx) => {
+	console.log(elem)
+})
 
 //Create and array of numbers
 //Return a new array of numbers that includes every even number from the previous Arrays
+numArr = [1,2,3,4,5]
+newArr = numArr.filter (x => x % 2 === 0)
+console.log(newArr)
 
 //Create a function that takes in an array of numbers
 //Alert the sum of the second lowest and the second highest number
+numArr2 = [10,3,1,4,5]
+numArr2.sort((a,b) => a > b ? 1 : -1)
+let sum = numArr2[1] + numArr2[numArr2.length-2]
+console.log(sum)
