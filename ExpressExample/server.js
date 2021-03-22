6fa9d47c3a1549b14ebf2a3fcd3ec444cf237782
Rapid1898 <markuspolzer@gmx.at>
2021-03-22 20:49:09 +0100

    Update

--- ExpressExample/server.js
+++ ExpressExample/server.js
@@ -5,19 +5,27 @@ const app = express();
 
 app.use(bodyParser.urlencoded({ extended: true }))
 
-// MongoClient.connect("mongodb+srv://Rapid1898:I65faueI65f@cluster0.ram23.mongodb.net/myFirstDatabase?retryWrites=true&w=majority", {
-//   useUnifiedTopology: true
-// }, (err, client) => {
-//   if (err) return console.error(err)
-//   console.log('Connected to Database')
-// })
-
 MongoClient.connect("mongodb+srv://Rapid1898:I65faueI65f@cluster0.ram23.mongodb.net/myFirstDatabase?retryWrites=true&w=majority", {
   useUnifiedTopology: true })
   .then(client => {
     console.log('Connected to Database')
     const db = client.db('star-wars-quotes')
+    const quotesCollection = db.collection('quotes')
+
+    app.get('/', (req, res) => {
+      res.sendFile(__dirname + '/index.html')
+    })
+
+    app.post('/quotes', (req, res) => {
+      quotesCollection.insertOne(req.body)
+        .then(result => {
+          console.log(result)
+        })
+        .catch(error => console.error(error))
+    })
   })
+
+
 
 app.listen(3000, function() {
   console.log('listening on 3000')
@@ -35,6 +43,14 @@ app.get('/', (req, res) => {
 //   console.log('Hellooooooooooooooooo!')
 // })
 
-app.post('/quotes', (req, res) => {
-  console.log(req.body)
-})
+// app.post('/quotes', (req, res) => {
+//   console.log(req.body)
+// })
+
+// app.post('/quotes', (req, res) => {
+//   quotesCollection.insertOne(req.body)
+//     .then(result => {
+//       console.log(result)
+//     })
+//     .catch(error => console.error(error))
+// })
