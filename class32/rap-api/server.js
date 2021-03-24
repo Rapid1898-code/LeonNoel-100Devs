f9d71d4f8b11c68a22a3a9b7073737af48a671bb
Rapid1898 <markuspolzer@gmx.at>
2021-03-24 23:34:28 +0100

    upd

--- class32/rap-api/server.js
+++ class32/rap-api/server.js
@@ -2,8 +2,18 @@ const express = require("express")
 const app = express()
 const PORT = 8000
 
+let savage = {
+    "age": 28,
+    "birthname": "Sayaa Bin Abraham-Joseph",
+    "birthLocation": "London, England"
+}
+
 app.get("/", (request, response) => {
     response.sendFile(__dirname + "/index.html")    
+})
+
+app.get("/api/savage", (request, response) => {
+    response.json(savage)
 })
 
 app.listen(PORT, () => {
