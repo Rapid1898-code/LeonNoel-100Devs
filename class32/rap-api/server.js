96418d3811843e681761c0670ee465e4b437f1f2
Rapid1898 <rapid1898@gmail.com>
2021-03-25 14:00:21 +0100

    upd

--- class32/rap-api/server.js
+++ class32/rap-api/server.js
@@ -1,19 +1,40 @@
 const express = require("express")
 const app = express()
+const cors = require("cors")
 const PORT = 8000
 
-let savage = {
-    "age": 28,
-    "birthname": "Sayaa Bin Abraham-Joseph",
-    "birthLocation": "London, England"
+app.use(cors())
+
+let rappers = {
+    "21 savage": {
+        "age": 28,
+        "birthname": "Sayaa Bin Abraham-Joseph",
+        "birthLocation": "London, England"
+    },
+    "chance the rapper": {
+        "age": 27,
+        "birthname": "Canchacelor Joe",
+        "birthLocation": "Chigaco, Illinois"
+    },
+    "unknown": {
+        "age": 47,
+        "birthname": "N/A",
+        "birthLocation": "N/A"
+    },
 }
 
 app.get("/", (request, response) => {
     response.sendFile(__dirname + "/index.html")    
 })
 
-app.get("/api/savage", (request, response) => {
-    response.json(savage)
+app.get("/api/rappers/:rapperName", (request, response) => {
+    const rapName = request.params.rapperName.toLowerCase()
+    console.log(rapName)
+    if (rappers[rapName]) {
+        response.json(rappers[rapName])
+    } else {
+        response.json(rappers["unknown"])
+    }    
 })
 
 app.listen(PORT, () => {
