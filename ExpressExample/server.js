ae628a62ea703dc2e79e788d842446378e16b5b8
Rapid1898 <markuspolzer@gmx.at>
2021-03-31 17:55:37 +0200

    class33

--- ExpressExample/server.js
+++ ExpressExample/server.js
@@ -7,39 +7,40 @@ app.use(bodyParser.urlencoded({ extended: true }))
 app.use(express.static('public'))
 app.use(bodyParser.json())
 
-MongoClient.connect("mongodb+srv://Rapid1898:I65faueI65f@cluster0.ram23.mongodb.net/myFirstDatabase?retryWrites=true&w=majority", {
+MongoClient.connect("mongodb+srv://ReadMongo:81468146@cluster0.ram23.mongodb.net/myFirstDatabase?retryWrites=true&w=majority", {
   useUnifiedTopology: true })
   .then(client => {
     console.log('Connected to Database')
     const db = client.db('star-wars-quotes')
-    const quotesCollection = db.collection('quotes')
-
-    app.get('/', (req, res) => {
-      db.collection('quotes').find().toArray()
-        .then(results => {
-          res.render('index.ejs', { quotes: results })
-        })
-        .catch(error => console.error(error))
-    })
-
-    app.post('/quotes', (req, res) => {
-      quotesCollection.insertOne(req.body)
-        .then(result => {
-          res.redirect('/')
-        })
-        .catch(error => console.error(error))
-    })
-
-    app.put('/quotes', (req, res) => {
-      quotesCollection.findOneAndUpdate(/* ... */)
-        .then(result => {
-          console.log(result)
-         })
-        .catch(error => console.error(error))
-    })
+    const quotesCollection = db.collection('quotes')  
   })
+  .catch(err => {                             
+    console.log(`error ${err}`)
+  })    
 
+app.get('/', (req, res) => {
+  db.collection('quotes').find().toArray()
+    .then(results => {
+      res.render('index.ejs', { quotes: results })
+    })
+    .catch(error => console.error(error))
+})
 
+app.post('/quotes', (req, res) => {
+  quotesCollection.insertOne(req.body)
+    .then(result => {
+      res.redirect('/')
+    })
+    .catch(error => console.error(error))
+})
+
+app.put('/quotes', (req, res) => {
+  quotesCollection.findOneAndUpdate(/* ... */)
+    .then(result => {
+      console.log(result)
+      })
+    .catch(error => console.error(error))
+})
 
 app.listen(3000, function() {
   console.log('listening on 3000')
--- class33/rapper-api/server.js
+++ class33/rapper-api/server.js
@@ -4,11 +4,18 @@ const MongoClient = require("mongodb").MongoClient
 const PORT = 2121
 
 let db, 
-    dbConnectionstr = "mongodb+srv://Rapid1898:I65faue#MB7#@cluster0.ram23.mongodb.net/myFirstDatabase?retryWrites=true&w=majority"
+    dbConnectionstr = "mongodb+srv://Rapid1898:I65faueI65f@cluster0.ram23.mongodb.net/myFirstDatabase?retryWrites=true&w=majority",
     dbName = "rap"
+
+console.log(db)
+console.log(dbConnectionstr)
+console.log(dbName)
+
 // connect to mongoDB
-MongoClient.connect(dbConnectionstr, {useUnifiedTopology: true})
+MongoClient.connect(dbConnectionstr, {
+    useUnifiedTopology: true})
     .then(client => {
         console.log(`Connected to ${dbName} Database`)
         db = client.db(dbName)
     })
+
