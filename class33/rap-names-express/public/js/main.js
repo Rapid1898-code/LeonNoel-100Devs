72565886cfcc896ce9b102da926632832ddbb7e5
Rapid1898 <markuspolzer@gmx.at>
2021-04-01 21:45:56 +0200

    upd

--- class33/rap-names-express/public/js/main.js
+++ class33/rap-names-express/public/js/main.js
@@ -1,10 +1,8 @@
 const deleteText = document.querySelectorAll('.fa-trash')
 const thumbText = document.querySelectorAll('.fa-thumbs-up')
-
 Array.from(deleteText).forEach((element)=>{
     element.addEventListener('click', deleteRapper)
 })
-
 Array.from(thumbText).forEach((element)=>{
     element.addEventListener('click', addLike)
 })
--- class33/rap-names-express/server.js
+++ class33/rap-names-express/server.js
@@ -4,22 +4,19 @@ const MongoClient = require('mongodb').MongoClient
 const PORT = 2121
 require('dotenv').config()
 
-
-let db,
-    dbConnectionStr = process.env.DB_STRING,
-    dbName = 'rap'
-
-MongoClient.connect(dbConnectionStr, { useUnifiedTopology: true })
-    .then(client => {
-        console.log(`Connected to ${dbName} Database`)
-        db = client.db(dbName)
-    })
-    
 app.set('view engine', 'ejs')
 app.use(express.static('public'))
 app.use(express.urlencoded({ extended: true }))
 app.use(express.json())
 
+let db,
+    dbConnectionStr = process.env.DB_STRING,
+    dbName = 'rap'
+MongoClient.connect(dbConnectionStr, { useUnifiedTopology: true })
+    .then(client => {
+        console.log(`Connected to ${dbName} Database`)
+        db = client.db(dbName)
+    }) 
 
 app.get('/',(request, response)=>{
     db.collection('rappers').find().sort({likes: -1}).toArray()
@@ -63,7 +60,6 @@ app.delete('/deleteRapper', (request, response) => {
         response.json('Rapper Deleted')
     })
     .catch(error => console.error(error))
-
 })
 
 app.listen(process.env.PORT || PORT, ()=>{
