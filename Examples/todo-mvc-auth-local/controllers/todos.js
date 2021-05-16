66258a5b8ddb61c3784ed068f031793d79d5164d
Rapid1898 <markuspolzer@gmx.at>
2021-05-16 12:33:28 +0200

    upd

--- Examples/todo-mvc-auth-local/controllers/todos.js
+++ Examples/todo-mvc-auth-local/controllers/todos.js
@@ -4,7 +4,7 @@ module.exports = {
     getTodos: async (req,res)=>{
         console.log(req.user)
         try{
-            const todoItems = await Todo.find({userId:req.user.id})
+            const todoItems = await Todo.find({userId:req.user.id})  
             const itemsLeft = await Todo.countDocuments({userId:req.user.id,completed: false})
             res.render('todos.ejs', {todos: todoItems, left: itemsLeft, user: req.user})
         }catch(err){
@@ -13,32 +13,44 @@ module.exports = {
     },
     createTodo: async (req, res)=>{
         try{
-            await Todo.create({todo: req.body.todoItem, completed: false, userId: req.user.id})
+            await Todo.create(
+                {
+                    todo: req.body.todoItem,
+                    completed: false, userId:
+                    req.user.id
+                }
+            )
             console.log('Todo has been added!')
             res.redirect('/todos')
-        }catch(err){
+        } catch(err){
             console.log(err)
         }
     },
     markComplete: async (req, res)=>{
         try{
-            await Todo.findOneAndUpdate({_id:req.body.todoIdFromJSFile},{
-                completed: true
-            })
+            await Todo.findOneAndUpdate(
+                {_id:req.body.todoIdFromJSFile},
+                {
+                    completed: true
+                }
+            )
             console.log('Marked Complete')
             res.json('Marked Complete')
-        }catch(err){
+        } catch(err){
             console.log(err)
         }
     },
     markIncomplete: async (req, res)=>{
         try{
-            await Todo.findOneAndUpdate({_id:req.body.todoIdFromJSFile},{
-                completed: false
-            })
+            await Todo.findOneAndUpdate(
+                {_id:req.body.todoIdFromJSFile},
+                {
+                    completed: false
+                }
+            )
             console.log('Marked Incomplete')
             res.json('Marked Incomplete')
-        }catch(err){
+        } catch(err){
             console.log(err)
         }
     },
